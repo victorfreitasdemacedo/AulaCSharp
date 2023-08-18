f5b094fec0c13069116e4c5d4b7f21f180d04823
Victor Freitas <victorfreitas1494@outlook.com>
2023-08-18 12:34:55 -0300

    aula de OOP da alura

---
+++ Musica.cs
@@ -0,0 +1,27 @@
+namespace Musica
+{
+    class Musicas
+    {
+        public string? Nome{get; set;}
+        public string? Artista{get; set;}
+        public int? Duracao{get; set;}
+        public bool Disponivel{get; set;}
+        
+
+
+        public void ExebirFichaTecnica()
+        {
+            Console.WriteLine($"Nome {Nome}");
+            Console.WriteLine($"Artista {Artista}");
+            Console.WriteLine($"Duração {Duracao}");
+            if (Disponivel)
+            {
+                Console.WriteLine($"Disponivel no plano");    
+            }else
+            {
+                Console.WriteLine("Adquira o plano plus+");
+            }
+            
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,16 @@
+﻿using Musica;
+namespace AulaCSharp
+{
+    class AulaCSharp
+    {
+       public static void Main(string[] args)
+        {       
+            Musicas musical = new Musicas();
+            musical.Nome = "Roxane";
+            musical.Artista = "The Police";
+            musical.Duracao = 273;
+            musical.Disponivel = true;
+            musical.ExebirFichaTecnica();
+        }
+    }
+}
